b15d2f513010d45c8c15e5ad373941194d150a21
TheoHorn <82098718+TheoHorn@users.noreply.github.com>
2024-03-20 15:07:25 +0100

    [INIT] dependecies to lucene and sqlite

--- code/src/main/java/module-info.java
+++ code/src/main/java/module-info.java
@@ -1,6 +1,13 @@
 module telecom.projet {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires lucene.analyzers.common;
+    requires lucene.core;
+    requires lucene.queryparser;
+    requires lucene.queries;
+    requires org.xerial.sqlitejdbc;
+    
 
     opens telecom.projet to javafx.fxml;
     exports telecom.projet;
