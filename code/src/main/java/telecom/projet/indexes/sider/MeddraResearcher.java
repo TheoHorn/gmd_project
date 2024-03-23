d04e960f967ca244ae9069d11c34ead86b029330
Helena <helena.barbillon@orange.fr>
2024-03-23 22:52:51 +0100

    Fonctions de recherche pour meddra

--- code/src/main/java/telecom/projet/indexes/sider/MeddraResearcher.java
+++ code/src/main/java/telecom/projet/indexes/sider/MeddraResearcher.java
@@ -2,6 +2,7 @@ package telecom.projet.indexes.sider;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -17,25 +18,89 @@ import org.apache.lucene.store.FSDirectory;
 
 public class MeddraResearcher {
     public static void main(String[] args) {
-        String dir_meddra_all_indications = "indexes/sider/meddra_all_indications";
-        String dir_meddra_all_se = "indexes/sider/meddra_all_se";
-        String dir_meddra_freq = "indexes/sider/meddra_freq";
-        String dir_meddra = "indexes/sider/meddra";
+        
+        //CID: code for a drug
 
-        //What we are looking for
-        String querystr = "syndrome";
-        String type_of_query = "side_effect";  //indication, umls, stitch_id
+        //print the CID associated to a CUI (CUI: code for a side effect)
+        System.out.println(getCIDbyCUI_meddra_all_indication("C2062908"));
 
-        try {
-            search(dir_meddra, querystr, type_of_query);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //print the CID associated to a side effect
+        System.out.println(getCIDbySideEffect_meddra_all_se("abdominal pain"));
+
+        //print the CID associated to an indication
+        System.out.println(getCIDbyIndication_meddra_all_indication("abdominal pain"));
+
     }
 
-    public static void search(String indexDirectoryPath, String querystr, String type_of_query) throws IOException {
+    public static ArrayList<String> getCIDbySideEffect_meddra_all_se(String query){
+        /*
+         * This method is used to get the CID by side effect
+         * @param se: the side effect to search for
+         * @return: list of CID
+         */
+        String indexDirectoryPath = "indexes/sider/meddra_all_se";
+        String type_of_query = "side_effect";
+        //results
+        ArrayList<String> cid = new ArrayList<String>(); //cid = UMLS
+        String type_of_result = "umls";//cid = UMLS
+
+        System.out.println("Searching for CID associated to side effect " + query);
         try {
-            System.out.println("Searching for: " + querystr);
+            cid = search(indexDirectoryPath, query, type_of_query, type_of_result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cid;
+    }
+
+    public static ArrayList<String> getCIDbyIndication_meddra_all_indication(String query){
+        /*
+         * This method is used to get the CID by indication
+         * @param indication: the indication to search for
+         * @return: list of CID
+         */
+        String indexDirectoryPath = "indexes/sider/meddra_all_indications";
+        String type_of_query = "indication";
+        //results
+        ArrayList<String> cid = new ArrayList<String>(); //cid = UMLS
+        String type_of_result = "umls";//cid = UMLS
+
+        System.out.println("Searching for CID associated to indication " + query);
+        try {
+            cid = search(indexDirectoryPath, query, type_of_query, type_of_result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cid;
+    }
+
+
+    public static ArrayList<String> getCIDbyCUI_meddra_all_indication(String query){
+        /*
+         * This method is used to get the CID by CUI
+         * @param cui: the CUI to search for
+         * @return: list of CID
+         */
+
+        ArrayList<String> cid = new ArrayList<String>(); //cid = UMLS
+        String indexDirectoryPath = "indexes/sider/meddra_all_indications";
+        String type_of_query = "umls";//cid = UMLS
+        String type_of_result = "stitch_id";//cui = STITCH ID
+
+        System.out.println("Searching for CID associated to CUI " + query);
+        try {
+            cid = search(indexDirectoryPath, query, type_of_query, type_of_result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cid;
+    }
+
+    public static ArrayList<String> search(String indexDirectoryPath, String querystr, String type_of_query, String type_of_result) throws IOException {
+        try {
+            ArrayList<String> result = new ArrayList<String>();
+            
+            //System.out.println("Searching for: " + querystr);
             Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
             IndexReader reader = DirectoryReader.open(indexDirectory);
             IndexSearcher searcher = new IndexSearcher(reader);
@@ -46,26 +111,21 @@ public class MeddraResearcher {
             int maxHitsPerPage = 100; // Maximum number of hits per page
             TopDocs results = searcher.search(query, maxHitsPerPage);
             ScoreDoc[] hits = results.scoreDocs;
-            System.out.println("Number of hits: " + hits.length + "  (Maximum: "+ maxHitsPerPage +")");
-            if (hits.length == 0) {
-                System.out.println("No results found.");
-            }else{
-                System.out.println("UMLS\t\tSTITCH ID\tIndication");
-            }
+
+            //System.out.println("Number of hits: " + hits.length + "  (Maximum: "+ maxHitsPerPage +")");
+            
             for (ScoreDoc hit : hits) {
                 Document doc = searcher.doc(hit.doc);
-                // Get the value of the field
-                String umls = doc.get("umls");
-                //String stitch_id = doc.get("stitch_id");
-                String indication = doc.get("side_effect");
-                //System.out.println(umls +"\t"+ stitch_id +"\t"+ indication);
-                
-                System.out.println(umls +"\t"+ indication);
+                String res = doc.get(type_of_result);// Get the value of the field
+                if (!result.contains(res)){
+                    result.add(res);
+                }    
             }
-
             reader.close();
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
